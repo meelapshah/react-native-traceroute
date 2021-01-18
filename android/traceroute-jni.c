29ce69a6e2ac5b5e2a39ad3f1cdbf156cb4dea73
Meelap Shah <meelapshah@gmail.com>
2021-01-18 12:02:58 -0800

    move traceroute to separate thread for isolation

--- android/traceroute-jni.c
+++ android/traceroute-jni.c
@@ -60,12 +60,36 @@ void perror(const char *msg) {
 
 void exit(int status) {
     LOGE("traceroute error to exit program, status:%d", status);
+struct doTracerouteArgs {
+    int argc;
+    char **argv;
+};
+
+void *doTraceroute(void *arg) {
+    JNIEnv *env = NULL;
+    if ((*jvm)->AttachCurrentThread(jvm, &env, NULL) != JNI_OK) {
+        LOGE("AttachCurrentThread failed");
+        return NULL;
+    }
+
+    struct doTracerouteArgs *a = (struct doTracerouteArgs *)arg;
+    int argc = a->argc;
+    char **argv = a->argv;
+
+    trace_return = runmain(argc, argv);
+
+    (*jvm)->DetachCurrentThread(jvm);
+    return NULL;
 }
 
 JNIEXPORT jint JNICALL
-Java_com_reactnativetraceroute_TracerouteModule_nativeTraceroute(JNIEnv *env, jclass type, jobjectArray jarray) {
+Java_com_reactnativetraceroute_TracerouteModule_nativeTraceroute(JNIEnv *env, jclass type, jobjectArray jarray, jobject resp) {
+    if (EBUSY == pthread_mutex_trylock(&trace_active)) {
+        LOGE("multiple concurrent traceroute not supported")
+        return -1;
+    }
+
     jint argc = (*env)->GetArrayLength(env, jarray);
-    LOGE("jarray has length %d", argc);
 
     jstring jargv[argc];
     char *argv[argc];
@@ -75,11 +99,23 @@ Java_com_reactnativetraceroute_TracerouteModule_nativeTraceroute(JNIEnv *env, jc
         LOGE("argv[%d] (len %d) = %s", i, strlen(argv[i]), argv[i]);
     }
 
-    int res = runmain(argc, argv);
+    respRef = (*env)->NewGlobalRef(env, resp);
+
+    pthread_t tid;
+    struct doTracerouteArgs args = {
+        .argc = (int) argc,
+        .argv = argv
+    };
+    pthread_create(&tid, NULL, doTraceroute, (void *)&args);
+    pthread_join(tid, NULL);
 
     for (int i = 0; i < argc; i++) {
         (*env)->ReleaseStringUTFChars(env, jargv[i], (const jchar *) argv[i]);
     }
 
-    return res;
+    (*env)->DeleteGlobalRef(env, respRef);
+
+    pthread_mutex_unlock(&trace_active);
+
+    return trace_return;
 }
